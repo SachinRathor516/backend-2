31691b6e51a3f4bb3b9bae4397365b36304009a6
Sachin Rathor <sachinrathor516@gamil.com>
2026-08-29 10:27:07 +0530

    day-9 update Api

--- day-9/Frontend/src/App.jsx
+++ day-9/Frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [notes, setNotes] = useState([])
 
   function fetchNotes() {
-    axios.get('http://localhost:3000/api/notes')
+    axios.get('https://day2-xsrq.onrender.com/api/notes')
 
       .then(res => {
         console.log(res.data);
@@ -24,7 +24,7 @@ function App() {
     const {title , description} = e.target.elements
     console.log(title.value ,description.value);
 
-    axios.post('http://localhost:3000/api/notes' , {
+    axios.post('https://day2-xsrq.onrender.com/api/notes' , {
       title:title.value,
       description:description.value
     })
@@ -40,7 +40,7 @@ function App() {
   function handleDeleteNote(noteId) {
     console.log(noteId);
 
-    axios.delete('http://localhost:3000/api/notes/'+noteId)
+    axios.delete('https://day2-xsrq.onrender.com/api/notes/'+noteId)
 
     .then(res=>{
       console.log(res.data);
